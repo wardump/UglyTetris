0a9816bbff76e7a4d1b269025cbc89a1d1dba023
Nikolay Shestakov <nikolay.shestakov@DPTechnology.com>
2020-09-29 12:43:47 +0700

    FieldHelper to a separate file
    removed usings

--- WpfApp1/Game.cs
+++ WpfApp1/Game.cs
@@ -1,6 +1,5 @@
 ﻿
 using System.Windows.Controls;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -63,74 +62,6 @@ namespace WpfApp1
                     w.TestFigure.Draw(20 * x, 20 * y);
                 }
             }
-        }
-    }
-
-    public class FieldHelper
-    {
-        public static int CheckLines(Rectangle[,] field)
-        {
-            var left = field.GetLowerBound(0) + 1;
-            var right = field.GetUpperBound(0) - 1;
-
-            var remove = 0;
-
-            for (var i = field.GetLowerBound(1); i < field.GetUpperBound(1); i++)
-            {
-                var isFull = true;
-                for (var x = left; x <= right ; x++) // check line
-                {
-                    if (field[x, i] == null)
-                    {
-                        isFull = false;
-                        break;
-                    }
-                }
-
-                if (isFull)
-                {
-                    // remove the line
-                    remove++;
-
-                    for (var x = left; x <= right; x++)
-                    {
-                        for (var y = i - 1; y >= 0; y--)
-                        {
-                            field[x, y + 1] = field[x, y];
-                        }
-                    }
-                }
-            }
-
-            return remove;
-        }
-
-        public static bool CheckFigure(Rectangle[,] field, Figure f, int figureX, int figureY)
-        {
-            var left = field.GetLowerBound(0) + 1;
-            var right = field.GetUpperBound(0) - 1;
-            var bottom = field.GetUpperBound(1) - 1;
-
-            for (var i = f.Tiles.GetLowerBound(0); i <= f.Tiles.GetUpperBound(0); i++)
-            {
-                for (var j = f.Tiles.GetLowerBound(1); j <= f.Tiles.GetUpperBound(1); j++)
-                {
-                    if (f.Tiles[i, j] != null)
-                    {
-                        if (figureX + i > right || figureX + i < 0 || figureY + j > bottom || figureY + j < 0)
-                        {
-                            return false;
-                        }
-
-                        if (field[figureX + i, figureY + j] != null)
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            return true;
         }
     }
 }
