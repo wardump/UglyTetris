517847db0ff2400a4113f254df78241fbbd2829d
Nikolay Shestakov <ShestakovNA@dp.dplabs.org>
2020-11-25 17:26:25 +0700

    Field RemoveLine test

--- Tests/FieldTest.cs
+++ Tests/FieldTest.cs
@@ -1,7 +1,78 @@
+using System.Collections.Generic;
+using System.Windows.Documents;
+using FluentAssertions;
+using UglyTetris.GameLogic;
+using Xunit;
+
 namespace Tests
 {
     public class FieldTest
     {
-        
+        private static Tile T() => new Tile("Brown");
+
+        private static List<Field> Fields = new List<Field>()
+        {
+            new Field(new Tile[,]
+            {
+                // 0 -> Y
+                // | 
+                // v X 
+                {T(), T(), T(), T(), T(), T(), T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {null, null, null, null, null, null, T(), T(),},
+                {T(), T(), T(), T(), T(), T(), T(), T(),},
+            }),
+            new Field(new Tile[,]
+                {
+                    // 0 -> Y
+                    // | 
+                    // v X 
+                    {T(), T(), T(), T(), T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, null, T(), T(), T(),},
+                    {T(), T(), T(), T(), T(), T(), T(), T(),},
+                }
+            ),
+            new Field(new Tile[,]
+                {
+                    // 0 -> Y
+                    // | 
+                    // v X 
+                    {T(), T(), T(), T(), T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), null, T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), null, T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, T(), T(), T(), T(),},
+                    {null, null, null, null, null, T(), T(), T(),},
+                    {T(), T(), T(), T(), T(), T(), T(), T(),},
+                }
+            )
+        };
+
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 0)]
+        public void RemoveLines(int fieldIndex, int lineCount)
+        {
+            var field = Fields[fieldIndex];
+            field.RemoveFullLines().Should().Be(lineCount);
+        }
     }
 }
